66c38362b897abc07966b938458945624fede7c9
uishnik <uishnik@yandex.ru>
2017-11-01 10:23:11 +0300

    csp 4 R3

--- compose/cpcsp/install.sh
+++ compose/cpcsp/install.sh
@@ -1,164 +1,264 @@
-#!/bin/bash
+#!/bin/sh
 
-function check_fail(){
-if test $1 -ne 0; then
- if test $UID -eq 0; then
-  echo "  Installation failed. LSB package may not be installed.
-  Install LSB package and reinstall CryptoPro CSP. If it does not help, please 
-  read installation documentation or contact the manufacturer: support@cryptopro.ru."
- fi
- exit $1
-fi	
+COMPAT_VERSION="1.0.0-1"
+VERSION="4.0.*-5"
+
+show_help() {
+    echo "\
+usage: ./install.sh [kc1|kc2] [package [...]]
+  kc1: install kc1 packages (by default)
+  kc2: install kc2 packages
+  [package [...]]: list of additional packages"
 }
 
-show_help(){
-  echo "
-  using install.sh: 
-  install.sh [arguments]:
-
-  select [arguments] from:
-  kc1 - install kc1 packages (by default)
-  kc2 - install kc2 packages
-"
+parse_args() {
+    enclosure="kc1"
+    case "$1" in
+        "kc1")
+            shift
+            ;;
+        "kc2")
+            enclosure="kc2"
+            shift
+            ;;
+        "-help"|"--help")
+            show_help
+            exit 0
+            ;;
+    esac
+    additional_packages="$*"
 }
 
-enclosure=kc1
-case $1 in
-""|kc1)
-  enclosure=kc1
-;;
-kc2)
-  enclosure=kc2
-;;
-"-help")
-  show_help
-  exit 0
-;;
-"--help")
-  show_help
-  exit 0
-;;
-*)
-  echo "usage: install.sh [kc1|kc2]"
-  exit 1
-;;
-esac
+which_architecture() {
+    machine_architecture="$(uname -m)"
+    case "${machine_architecture}" in
+        "x86_64"|"amd64"|"ppc64"|"ppc64le")
+            bits_postfix="-64"
+            ;;
+        *)
+            bits_postfix=""
+            ;;
+    esac
+    case "${machine_architecture}" in
+        "armv7l"|"armv7")
+            is_arm=1
+            ;;
+        *)
+            is_arm=0
+            ;;
+    esac
+}
 
-cd `dirname $0`
-
-[ -r /etc/debian_version ] && debian=1
-[ -r /etc/lsb-release ] && grep -q Ubuntu /etc/lsb-release && debian=1
-
-is_deb_release=0
-if test x$debian = x1; then
-  ls lsb-cprocsp-base*.deb > /dev/null 2>&1 && is_deb_release=1
-  if test $is_deb_release -eq 1; then 
-    type dpkg 1>/dev/null 2>&1
-    if test $? -ne 0; then
-      echo "You are trying to install debian packages on not debian package system" && exit 1
-    fi
-  fi
-fi
-
-/bin/sh ./uninstall.sh || exit $?
-
-isarm=0
-case `uname -m` in
-x86_64|amd64)
-  ARG=64
-  if test $is_deb_release -eq 1; then
-    arch=amd64
-  else
-    arch=x86_64
-  fi
-  ;;
-armv7l|armv7)
-  arch=noarch
-  isarm=1
-  ;;
-ppc64)
-  ARG=64
-  arch=ppc64
-  ;;
-ppc64le)
-  ARG=64
-  arch=ppc64le
-  ;;
-*)
-  if test $is_deb_release -eq 1; then
-    arch=i386
-  else
-    arch=i486
-  fi
-  ;;
-esac
-
-
-if [ -f /etc/cp-release ]; then
-   cat /etc/cp-release | grep Gaia > /dev/null
-   isGaia=$((!$?))
-   if [ "$isGaia" -eq 1 ]; then
-      #GAiA OS
-      rpm -i cprocsp-compat-gaia-1.0.0-1.noarch.rpm
-   else
-      #SPLAT OS
-      rpm -i cprocsp-compat-splat-1.0.0-1.noarch.rpm
-   fi
-fi
-
-if [ -f /etc/altlinux-release ]; then
-  if test "$ARG" == "64"; then 
-     bits="-64"
-  fi
-  rpm -i cprocsp-compat-altlinux${bits}-1.0.0-1.noarch.rpm
-fi
-
-if test $is_deb_release -eq 1; then
-  if ! dpkg -s lsb-core > /dev/null 2>&1; then
-    echo 'Warning: lsb-core package not installed - installing cprocsp-compat-debian.'
-    echo 'If you prefer to install system lsb-core package then'
-    echo ' * uninstall CryptoPro CSP'
-    echo ' * install lsb-core manually'
-    echo ' * install CryptoPro CSP again'
-    dpkg -i cprocsp-compat-debian_1.0.0-1_all.deb || check_fail $?
-  fi
-
-  echo "Installing lsb-cprocsp-base_4.0.0-4_all.deb..."
-  dpkg -i lsb-cprocsp-base_4.0.0-4_all.deb || check_fail $?
-else
-  echo "Installing lsb-cprocsp-base-4.0.0-4.noarch.rpm..."
-  if test x$debian = x1; then
-    if [ $isarm -eq 1 ]; then
-      alien -kci cprocsp-compat-armhf-1.0.0-1.noarch.rpm || check_fail $?
-    fi
-    alien -kci lsb-cprocsp-base-4.0.0-4.noarch.rpm || check_fail $?
-  else
-    if [ $isarm -eq 1 ]; then
-      rpm -i cprocsp-compat-armhf-1.0.0-1.noarch.rpm || check_fail $?
-    fi
-    rpm -i lsb-cprocsp-base-4.0.0-4.noarch.rpm || check_fail $?
-  fi
-fi
-
-list="lsb-cprocsp-rdr lsb-cprocsp-capilite lsb-cprocsp-$enclosure cprocsp-curl"
-if test $is_deb_release -eq 1; then
-  for i in $list; do
-    if test x$ARG = x64 && [ -e lsb-cprocsp-rdr-64_4.0.0-4_$arch.deb ]; then
-      i="${i}-64"
-    fi
-    echo "Installing $i..."
-    dpkg -i ${i}_4.0.0-4_$arch.deb || check_fail $?
-  done
-else
-  for i in $list; do
-    if test x$ARG = x64 && [ -e lsb-cprocsp-rdr-64-4.0.0-4.$arch.rpm ]; then
-      i="${i}-64"
-    fi
-    echo "Installing $i..."
-    if test x$debian = x1; then
-      alien -kci $i-4.0.0-4.$arch.rpm || check_fail $?
+check_if_debian_system() {
+    if [ -f /etc/debian_version ] ||
+        grep Ubuntu /etc/lsb-release > /dev/null 2>&1
+    then
+        is_debian_system=1
     else
-      rpm -i $i-4.0.0-4.$arch.rpm || check_fail $?
+        is_debian_system=0
     fi
-  done
-fi
+}
+
+check_release_attributes() {
+    if ls ./lsb-cprocsp-base*.deb > /dev/null 2>&1 ; then
+        is_debian_release=1
+    else
+        is_debian_release=0
+    fi
+    if ls ./lsb-cprocsp-rdr-64* > /dev/null 2>&1 ; then
+        is_64_release=1
+    else
+        is_64_release=0
+    fi
+}
+
+# Use dpkg or alien on debian systems, otherwise use rpm.
+set_inst_cmd() {
+    if [ "${is_debian_system}" -eq 1 ] ; then
+        if [ "${is_debian_release}" -eq 1 ] ; then
+            inst_cmd="dpkg -i"
+        else
+            inst_cmd="alien -kci"
+        fi
+    else
+        if [ "${is_debian_release}" -eq 1 ] ; then
+            echo "Error: you are trying to install debian packages on not debian package system" >&2
+            exit 1
+        else
+            inst_cmd="rpm -i"
+        fi
+    fi
+}
+
+# The release variables are used to construct full names of packages.
+set_release_variables() {
+    if [ "${is_debian_release}" -eq 1 ] ; then
+        first_delimeter="_"
+        noarch="all"
+        second_delimeter="_"
+        extension=".deb"
+    else
+        first_delimeter="-"
+        noarch="noarch"
+        second_delimeter="."
+        extension=".rpm"
+    fi
+    case "${machine_architecture}" in
+        # Enforce to install 64-bit packages on 64-bit system.
+        "x86_64"|"amd64")
+            if [ "${is_debian_release}" -eq 1 ] ; then
+                arch="amd64"
+            else
+                arch="x86_64"
+            fi
+            ;;
+        "ppc64"|"ppc64le")
+            arch="${machine_architecture}"
+            ;;
+        "armv7l"|"armv7"|"mips")
+            arch="${noarch}"
+            ;;
+        *)
+            if [ "${is_debian_release}" -eq 1 ] ; then
+                arch="i386"
+            elif ls ./*.i686.rpm > /dev/null 2>&1 ; then
+                arch="i686"
+            else
+                arch="i486"
+            fi
+            ;;
+    esac
+}
+
+lsb_warning() {
+    echo "Warning: lsb-core package not installed - installing cprocsp-compat-debian.
+If you prefer to install system lsb-core package then
+ * uninstall CryptoPro CSP
+ * install lsb-core manually
+ * install CryptoPro CSP again" >&2
+}
+
+construct_compat_package() {
+    if [ -f /etc/cp-release ] ; then
+        if grep Gaia /etc/cp-release > /dev/null 2>&1 ; then
+            _distr="gaia"
+        else
+            _distr="splat"
+        fi
+    elif [ -f /etc/altlinux-release ] ; then
+        _distr="altlinux${bits_postfix}"
+    elif [ -f /etc/os-rt-release ] ; then
+        _distr="osrt${bits_postfix}"
+    elif [ "${is_arm}" -eq 1 ] ; then
+        _distr="armhf"
+    elif [ "${is_debian_system}" -eq 1 ] ; then
+        if dpkg -s lsb-core > /dev/null 2>&1 ; then
+            compat_package=""
+            return
+        else
+            lsb_warning
+            _distr="debian"
+        fi
+    else
+        compat_package=""
+        return
+    fi
+
+    compat_package="cprocsp-compat-\
+${_distr}\
+${first_delimeter}\
+${COMPAT_VERSION}\
+${second_delimeter}\
+${noarch}\
+${extension}"
+}
+
+construct_other_packages() {
+    other_packages=""
+
+    _names="lsb-cprocsp-base \
+lsb-cprocsp-rdr lsb-cprocsp-${enclosure} lsb-cprocsp-capilite cprocsp-curl \
+lsb-cprocsp-ca-certs \
+${additional_packages}"
+
+    for _name in ${_names} ; do
+        _package="${_name}"
+
+        if [ "${is_64_release}" -eq 1 ] ; then
+            _package="${_package}${bits_postfix}"
+        fi
+
+        _package="${_package}\
+${first_delimeter}\
+${VERSION}\
+${second_delimeter}\
+${arch}\
+${extension}"
+
+        # There are several packages which are NOT architecture-specific,
+        # e.g. lsb-cprocsp-base, lsb-cprocsp-ca-certs and devel-packages.
+        # If the architecture-specific package is not found, try to install
+        # the noarch package.
+        # shellcheck disable=SC2086
+        if ! [ -f ${_package} ] ; then
+            _package="${_name}\
+${first_delimeter}\
+${VERSION}\
+${second_delimeter}\
+${noarch}\
+${extension}"
+        fi
+
+        # Even the noarch package wasn't found.
+        # shellcheck disable=SC2086
+        if ! [ -f ${_package} ] ; then
+            echo "Error: compatible package ${_name} doesn't exist or isn't unique" >&2
+            exit 1
+        fi
+
+        other_packages="${other_packages} ${_package}"
+    done
+}
+
+construct_list_of_packages() {
+    packages=""
+
+    construct_compat_package
+    packages="${packages} ${compat_package}"
+
+    # Other packages are the base packages and additional packages
+    # specified by command-line arguments.
+    construct_other_packages
+    packages="${packages} ${other_packages}"
+}
+
+check_fail() {
+    echo "Error: installation failed. LSB package may not be installed.
+      Install LSB package and reinstall CryptoPro CSP. If it does not help, please 
+      read installation documentation or contact the manufacturer: support@cryptopro.ru." >&2
+    exit "$1"
+}
+
+install_packages() {
+    for _package in ${packages} ; do
+        echo "Installing ${_package}..."
+        ${inst_cmd} "${_package}" || check_fail "$?"
+    done
+}
+
+main() {
+    cd "$(dirname "$0")" || exit "$?"
+    parse_args "$@"
+
+    which_architecture
+    check_if_debian_system
+    check_release_attributes
+    set_inst_cmd
+    set_release_variables
+    construct_list_of_packages
+
+    sh ./uninstall.sh || exit "$?"
+    install_packages
+}
+
+main "$@"
--- compose/cpcsp/uninstall.sh
+++ compose/cpcsp/uninstall.sh
@@ -1,28 +1,57 @@
 #!/bin/sh
 
-[ -r /etc/debian_version ] && debian=1
-[ -r /etc/lsb-release ] && grep -q Ubuntu /etc/lsb-release && debian=1
+check_if_debian_system() {
+    if [ -f /etc/debian_version ] ||
+        grep Ubuntu /etc/lsb-release > /dev/null 2>&1
+    then
+        is_debian_system=1
+    else
+        is_debian_system=0
+    fi
+}
 
-if test x$debian = x1; then
-  list_command="dpkg -l |grep -e rtSupCP -e cprocsp|awk '{print \$2}'"
-  del_command="dpkg -P"
-else
-  list_command="rpm -qa |grep -e rtSupCP -e cprocsp"
-  del_command="rpm -e --allmatches"
-fi
+set_del_command_and_package_lists() {
+    if [ "${is_debian_system}" -eq 1 ] ; then
+        pkglist="$(dpkg -l | grep -e rtSupCP -e cprocsp | awk '{print $2}')"
+        del_command="dpkg -P"
+    else
+        pkglist="$(rpm -qa | grep -e rtSupCP -e cprocsp)"
+        del_command="rpm -e --allmatches"
+    fi
+    rdr_package="$(echo "${pkglist}" | grep lsb-cprocsp-rdr | grep -v -e accord -e sobol)"
+    base_package="$(echo "${pkglist}" | grep base | grep -v ssl)"
+    compat_package="$(echo "${pkglist}" | grep compat)"
+    csp_packages="$(
+        echo "${pkglist}" \
+            | grep -vx -e "${rdr_package}" -e "${base_package}" -e "${compat_package}" \
+            | tr '\n' ' '
+    )"
+}
 
-pkglist=`eval "$list_command"`
-compat_pkg=`echo "$pkglist" | grep compat || echo 'NULL'`
-rdr_pkgs=`echo "$pkglist" | grep rdr-[0-9]`
-base_pkg=`echo "$pkglist" | grep base | grep -v ssl`
-pkglist=`echo "$pkglist" | grep -vx "$base_pkg" | grep -v "$compat_pkg"`
-for rdr_pkg in $rdr_pkgs; do pkglist=`echo "$pkglist" | grep -vx "$rdr_pkg"`; done
-if test -n "$pkglist"
-then
-  $del_command $pkglist
-fi
-if test -n "$rdr_pkgs"; then $del_command $rdr_pkgs; fi
-if test -n "$base_pkg"; then $del_command $base_pkg; fi
-if test "$compat_pkg" != "NULL"; then $del_command $compat_pkg; fi
+check_fail() {
+    echo "Error: failed to uninstall CSP packages" >&2
+    exit "$1"
+}
 
-exit $?
+main() {
+    check_if_debian_system
+    set_del_command_and_package_lists
+    if [ -n "${csp_packages}" ] ; then
+        # shellcheck disable=SC2086
+        ${del_command} ${csp_packages} || check_fail "$?"
+    fi
+    if [ -n "${rdr_package}" ] ; then
+        # shellcheck disable=SC2086
+        ${del_command} ${rdr_package} || check_fail "$?"
+    fi
+    if [ -n "${base_package}" ] ; then
+        # shellcheck disable=SC2086
+        ${del_command} ${base_package} || check_fail "$?"
+    fi
+    if [ -n "${compat_package}" ] ; then
+        # shellcheck disable=SC2086
+        ${del_command} ${compat_package} || check_fail "$?"
+    fi
+}
+
+main "$@"
